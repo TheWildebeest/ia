00d4ce158e4d64594cae8cd73ba4eaa4c533ea85
David Wildman <david@getlivesmart.com>
2022-09-04 21:17:57 +0100

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgbCustomModule } from 'src/ngb-custom/ngb-custom.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,6 +19,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbCustomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/ngb-custom/ngb-custom.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    NgbAccordionModule
+  ],
+  exports: [
+    NgbAccordionModule
+  ]
+})
+export class NgbCustomModule { }
