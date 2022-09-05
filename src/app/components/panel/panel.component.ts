3d381214aa03d0bea6516c56d0f97fc66be766d0
David Wildman <david@getlivesmart.com>
2022-09-05 10:09:51 +0100

    fix rendering

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -3,8 +3,7 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent implements OnInit {
 
